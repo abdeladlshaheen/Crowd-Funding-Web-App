36b22e62d142878c486cb6c439911008ccda7909
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-05-04 11:32:16 +0200

    view for reset password email request

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.exceptions import AuthenticationFailed
 # from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ResetPasswordEmailRequestSerializer
 from .models import User
 
 from rest_framework.views import APIView
@@ -10,6 +10,13 @@ from django_email_verification import send_email
 import jwt
 import datetime
 
+from rest_framework import generics, status, permissions
+from .utils import Util
+from django.urls import reverse
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 # class UserViewSet(viewsets.ModelViewSet):
 #     queryset = User.objects.all()
@@ -88,3 +95,29 @@ class LogoutView(APIView):
                 "detail": "You are not logged in!"
             }
         return response
+
+class RequestPasswordResetEmail(generics.GenericAPIView):
+    serializer_class = ResetPasswordEmailRequestSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        email = request.data.get('email', '')
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            current_site = get_current_site(
+                request=request).domain
+            relativeLink = reverse(
+                'password-reset-confirm', kwargs={'uidb64': uidb64, 'token': token})
+
+            redirect_url = request.data.get('redirect_url', '')
+            absurl = 'http://'+current_site + relativeLink
+            email_body = 'Hello, \n Use link below to reset your password  \n' + \
+                absurl+"?redirect_url="+redirect_url
+            data = {'email_body': email_body, 'to_email': user.email,
+                    'email_subject': 'Reset your passsword'}
+            Util.send_email(data)
+        return Response({'success': 'We have sent you a link to reset your password'}, status=status.HTTP_200_OK)
