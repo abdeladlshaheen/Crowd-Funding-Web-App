cc2da63742c1775a6b04a070308793232a8c9161
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-05-04 11:36:09 +0200

    serializer for set new password

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import User
 from django_countries.serializers import CountryFieldMixin
 from django_countries.serializer_fields import CountryField
-
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from rest_framework.exceptions import AuthenticationFailed
 
 class UserSerializer(CountryFieldMixin, serializers.ModelSerializer):
     class Meta:
@@ -38,3 +41,33 @@ class ResetPasswordEmailRequestSerializer(serializers.Serializer):
     class Meta:
         model = User
         fields = ['email']
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(
+        min_length=6, max_length=68, write_only=True)
+    token = serializers.CharField(
+        min_length=1, write_only=True)
+    uidb64 = serializers.CharField(
+        min_length=1, write_only=True)
+
+    class Meta:
+        fields = ['password', 'token', 'uidb64']
+
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('The reset link is invalid', 401)
+
+            user.set_password(password)
+            user.save()
+
+            return (user)
+        except Exception as e:
+            raise AuthenticationFailed('The reset link is invalid', 401)
+        return super().validate(attrs)
