a9d4e9b0cf01c625f26454df06c1fb901a10fefe
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-05-06 22:43:45 +0200

    urls for comments added

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
+import imp
 from django.urls import path, include
-from .views import CreateProjectView, ProjectDetails, ProjectListView, RateProjectView
+from .views import CreateProjectView, ProjectDetails, ProjectListView, RateProjectView, CommentListAPIView, CommentDetailAPIView
 from rest_framework import routers
 from .views import ProjectViewSet
+from django.urls import re_path
 
 router = routers.DefaultRouter()
 router.register('', ProjectViewSet)
@@ -11,5 +13,8 @@ urlpatterns = [
     # path('', ProjectListView.as_view()),
     path('create', CreateProjectView.as_view()),
     path('<int:id>/rate', RateProjectView.as_view()),
-    path('<int:id>', ProjectDetails.as_view())
-]
+    path('<int:id>', ProjectDetails.as_view()),
+    #path("comment/<int:pid>", Comment.as_view(), name="comment"),
+    re_path(r'^comments/$', CommentListAPIView.as_view(), name='List'), 
+    re_path(r'^comments/(?P<id>\d+)/$', CommentDetailAPIView.as_view(), name='thread'),
+]   
